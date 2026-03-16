bf4841ab1af81d77238230e59d99fea078de4d5c
borisey <51847223+borisey@users.noreply.github.com>
2026-03-16 09:35:42 +0300

    fix

--- views/site/index.php
+++ views/site/index.php
@@ -9,17 +9,14 @@ use yii\helpers\Url;
 
     <div class="jumbotron text-center bg-transparent mt-5 mb-5">
         <h1 class="display-4">Адреса</h1>
+
+        <h2>Оптимизация на уровне MySQL</h2>
     </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-12 mb-3">
-<!--                --><?php //foreach ($addresses as $address): ?>
-<!--                    <p>--><?php //= $address ?><!--</p>-->
-<!--                --><?php //endforeach; ?>
-
-
                 <table class="table">
                     <tr>
                         <th>ID</th>
